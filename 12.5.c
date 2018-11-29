909ac6b597a576581ca1e29a3dfd1791e0d1cbfc
ramu114 <ullerstyre@gmail.com>
2018-11-29 08:02:02 +0100

    Update 12.5.c

--- 12.5.c
+++ 12.5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//Function to reverse word/sentence with pointers
 void reverse(char *begin,char *end){
     char temp;
     while(begin>end){
@@ -9,13 +10,14 @@ void reverse(char *begin,char *end){
     }
 }
 
+//Get the length of the sentence
 int getLength(char *s){
     int counter;
     char* begin;
     
     begin = s;
     
-    while(*begin != '\0'){
+    while(*begin != '\0'){ //'\0' is the hidden end of the array.
         counter++;
         begin++;
     }
@@ -24,35 +26,37 @@ int getLength(char *s){
 
 int main(void)
 {
+	/*Variabler*/
     char s[500];
     char last;
     int i,j, length;
     char *begin=s;
     char *end=s;
     
+	/*Input*/
     printf("Enter a sentence: ");
     gets(s);
-    printf("\n");
     length=getLength(s);
     begin=&s[length-1];
     
+	/*Loops to flip sentence, and then flip the words*/
     for (i=length-1; i>=0; i--){
-        if (*(end+i)=='!' || *(end+i)=='?' || *(end+i)=='.'){
+        if (*(end+i)=='!' || *(end+i)=='?' || *(end+i)=='.'){ /*find end of sentence and save it*/
             last = *(end+i);
             *(end+i)=' ';
         }
         if (*(end+i)==' '){
-            //printf("------flip word------\n"); //MELLEMREGNINGER PRINT
+            //printf("------flip word------\n"); //Prototypes PRINT
             reverse(begin,(end+i+1));
             begin = end+i-1;
-            /*for (j=length;j>=0;j--){ //MELLEMREGNINGER PRINT
+            /*for (j=length;j>=0;j--){ //Prototypes PRINT
                 printf("%c",s[j]);
             }
             printf("\n");*/
         } else if(i==0){
-            //printf("------flip word------\n"); //MELLEMREGNINGER PRINT
+            //printf("------flip word------\n"); //Prototypes PRINT
             reverse(begin,(end+i));
-            /*for (j=length;j>=0;j--){     //MELLEMREGNINGER PRINT
+            /*for (j=length;j>=0;j--){     //Prototypes PRINT
                 printf("%c",s[j]);
             }
             printf("\n");*/
@@ -64,5 +68,3 @@ int main(void)
     }
     printf("%c\n\n", last);
 }
-
- 
